2ccb8242b38dcaf5b6f99d935087f3e48cc3c2c2
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2024-12-10 20:08:18 +0000

    Add card shuffling and interleaving logic
    Introduced an `InterleaveSequenceWith` extension method to interleave two sequences. Implemented card deck generation, shuffling, and display logic using LINQ queries and the new extension method in `Program.cs`. This enhances functionality for creating and manipulating a deck of playing cards.

---
+++ ConsolePlayingCards/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsolePlayingCards;
+
+public static class Extensions
+{
+    public static IEnumerable<T> InterleaveSequenceWith<T>
+        (this IEnumerable<T> first, IEnumerable<T> second)
+    {
+        var firstIter = first.GetEnumerator();
+        var secondIter = second.GetEnumerator();
+
+        while (firstIter.MoveNext() && secondIter.MoveNext())
+        {
+            yield return firstIter.Current;
+            yield return secondIter.Current;
+        }
+    }
+}
--- ConsolePlayingCards/Program.cs
+++ ConsolePlayingCards/Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsolePlayingCards;
 
-Console.WriteLine("Hello, World!");
+// query syntax
+var startingDeck = from s in Suits()
+    from r in Ranks()
+    select new { Suit = s, Rank = r };
+
+// method syntax
+// var startingDeck = Suits()
+//     .SelectMany(suit => Ranks()
+//         .Select(rank => new { Suit = suit, Rank = rank })
+// );
+
+// Display each card that we've generated and placed in startingDeck in the console
+foreach (var card in startingDeck)
+{
+    Console.WriteLine(card);
+}
+
+Console.WriteLine();
+
+var top = startingDeck.Take(26);
+var bottom = startingDeck.Skip(26);
+var shuffle = top.InterleaveSequenceWith(bottom);
+
+foreach (var c in shuffle)
+{
+    Console.WriteLine(c);
+}
+
+return;
+
+static IEnumerable<string> Ranks()
+{
+    yield return "two";
+    yield return "three";
+    yield return "four";
+    yield return "five";
+    yield return "six";
+    yield return "seven";
+    yield return "eight";
+    yield return "nine";
+    yield return "ten";
+    yield return "jack";
+    yield return "queen";
+    yield return "king";
+    yield return "ace";
+}
+
+static IEnumerable<string> Suits()
+{
+    yield return "clubs";
+    yield return "diamonds";
+    yield return "hearts";
+    yield return "spades";
+}
